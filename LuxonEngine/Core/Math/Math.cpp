e405a15b20c575d3da3b72e7dd400cdb94f3b578
LambShaun <Linksff03@gmail.com>
2025-06-12 16:30:47 +0800

    Vec2D

---
+++ LuxonEngine/Core/Math/Math.cpp
@@ -0,0 +1,15 @@
+#include "Math.h"
+
+#include <cmath>
+
+namespace Luxon {
+namespace Core {
+namespace Math {
+
+	float Sqrt(float number) {
+		return std::sqrt(number);
+		}
+
+} // namespace Math
+} // namespace Core
+} // namespace Luxon
---
+++ LuxonEngine/Core/Math/Math.h
@@ -0,0 +1,17 @@
+/**
+ * @file	Math.h
+ * @brief	ÊýÑ§¿â·â×°
+ * @author	LambShaun
+ **/
+
+#pragma once
+
+namespace Luxon {
+namespace Core {
+namespace Math {
+	
+	float Sqrt(float number);
+
+} // namespace Math
+} // namespace Core
+} // namespace Luxon
