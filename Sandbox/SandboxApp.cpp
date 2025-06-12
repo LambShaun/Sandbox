eaeca5bd362e0cf5b6afeff7255bf1088492b473
LambShaun <Linksff03@gmail.com>
2025-06-12 13:39:39 +0800

    use Input

--- Sandbox/SandboxApp.cpp
+++ Sandbox/SandboxApp.cpp
@@ -1,7 +1,8 @@
 #include "Platform/Window.h"
 #include "Function/Rendering/Renderer.h"
-#include <memory>
+#include "Function/Input/Input.h"
 
+#include <memory>
 #include <Windows.h>
 
 class SandboxApp {
@@ -25,10 +26,29 @@ public:
 					TranslateMessage(&Msg);
 					DispatchMessage(&Msg);
 			} else {
+				// MOVE
+				float speed = 1.0f;
+				if (Luxon::Input::isKeyPressed('W')) {
+					m_RectY -= speed;
+				}
+
+				if (Luxon::Input::isKeyPressed('S')) {
+					m_RectY += speed;
+				}
+
+				if (Luxon::Input::isKeyPressed('A')) {
+					m_RectX -= speed;
+				}
+
+				if (Luxon::Input::isKeyPressed('D')) {
+					m_RectX += speed;
+				}
+
+				// Renderer
 				Luxon::Renderer::BeginScene();
 				Luxon::Renderer::SetClearColor({ 255, 30, 30, 255 });
 				Luxon::Renderer::Clear();
-				Luxon::Renderer::DrawRect(100, 200, 100, 100, { 0,0,255,255 });
+				Luxon::Renderer::DrawRect(m_RectX, m_RectY, 100, 100, { 0,0,255,255 });
 				Luxon::Renderer::EndScene();
 			}
 		}
@@ -36,6 +56,9 @@ public:
 
 private:
 	std::unique_ptr<Luxon::Window> m_Window;
+
+	float m_RectY = 100.f;
+	float m_RectX = 100.f;
 };
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
 	SandboxApp* app = new SandboxApp();
